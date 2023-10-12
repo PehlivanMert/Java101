e3795ca72fd234067fd19c9439f0ab19cf90ab57
Mert <133760259+PehlivanMert@users.noreply.github.com>
2023-10-12 23:28:20 +0300

    Update dikdörtgenAlaniHesaplama.java

--- src/dikdörtgenAlaniHesaplama.java
+++ src/dikdörtgenAlaniHesaplama.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class dikdörtgenAlaniHesaplama {
+public class DikdörtgenAlaniHesaplama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Dikdörtgenin kısa kenarını giriniz: ");
