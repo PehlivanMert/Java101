3b4ddf6fbc3435299ca9a8184b34563636cdb4ed
PehlivanMert <pehlivannmert@gmail.com>
2023-10-16 06:02:17 +0300

    Değişken Tipleri İle Örnekler

---
+++ src/Degiskenler.java
@@ -0,0 +1,41 @@
+//Değişkenlerin Listesi
+public class Degiskenler {
+    public static void main(String[] args) {
+        byte baytSayi = 5;
+        short kisaSayi = 6;
+        long uzunSayi = 7;
+        int sayi = 8;
+        double ondalikliSayi = 5.5;
+        float ondalikliSayi2 = 5.5f;
+        float ondalikliSayi3 = 5.6F ;
+        char karakter = 'S';
+        char karakter2 = 240 ;
+        String  metin = "Merhaba";
+        boolean dogruMu = true;
+        System.out.println("Bayt sayı: " + baytSayi);
+        System.out.println("Kısa sayı: " + kisaSayi);
+        System.out.println("Uzun sayı: " + uzunSayi);
+        System.out.println("Sayı: " + sayi);
+        System.out.println("Ondalıklı sayı: " + ondalikliSayi);
+        System.out.println("Ondalıklı sayı 2: " + ondalikliSayi2);
+        System.out.println("Ondalıklı sayı 3: " + ondalikliSayi3);
+        System.out.println("Karakter: " + karakter);
+        System.out.println("Karakter 2: " + karakter2);
+        System.out.println("Metin: " + metin);
+        System.out.println("Doğru mu: " + dogruMu);
+
+
+
+
+        //Char veri tipi ile String Bir Metin yazdırma
+
+
+        char karakter3 = 'R';
+        char karakter4 = 'A';
+        char karakter5 = 'B';
+        char karakter6 = 'İ';
+        char karakter7 = 'A';
+System.out.println("" + karakter3 + karakter4 + karakter5 + karakter6 + karakter7);
+
+    }
+}
