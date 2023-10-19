8863c1f6efbbb4f8f7c9fa752a3a397e8602349f
PehlivanMert <pehlivannmert@gmail.com>
2023-10-19 11:40:44 +0300

    Açıklama

--- src/DikUcgeninHipotenusu.java
+++ src/DikUcgeninHipotenusu.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 public class DikUcgeninHipotenusu {
     public static void main(String[] args) {
+
+        //Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
         Scanner input=new Scanner(System.in);
         System.out.print("1. Kenar Uzunluğunu Giriniz: ");
         int birinciKenar = input.nextInt();
@@ -12,3 +15,8 @@ public class DikUcgeninHipotenusu {
         System.out.println("Hipotenüs: " + hipotenus);
     }
 }
+
+/* @Author: Mert PEHLIVAN
+ Date: 19.10.2023 Tuesday
+ IDE: intellij idea
+ */
