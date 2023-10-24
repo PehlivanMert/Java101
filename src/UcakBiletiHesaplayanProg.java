8916869c15323c00edbd2af0a4930d953d564e29
PehlivanMert <pehlivannmert@gmail.com>
2023-10-24 14:37:36 +0300

    İf-Else Ve Switch-Case ile Uçak Bilet Fiyat Uygulaması Denendi.

---
+++ src/UcakBiletiHesaplayanProg.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+public class UcakBiletiHesaplayanProg {
+    public static void main(String[] args) {
+        int yas, tip, km;
+        double tutar;
+
+
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.print("Yaşınızı Giriniz : ");
+
+        yas = girdi.nextInt();
+
+        System.out.print("Km'yi giriniz: ");
+
+        km = girdi.nextInt();
+
+        System.out.print("Tek yön için 1 ; Gidiş Dönüş için 2; ");
+
+        tip = girdi.nextInt();
+
+        tutar = km * 0.10;
+
+        double indirim1 = tutar - (tutar * 0.50);
+        double indirim2 = tutar - (tutar * 0.10);
+        double indirim3 = tutar - (tutar * 0.30);
+
+
+        switch (tip) {
+
+            case 1:
+
+                if (km > 0) {
+
+
+
+                    if ( yas > 0 && yas < 12) {
+
+                        System.out.println("Bilet Ücretiniz: " + indirim1 + "TL");
+
+                    } else if (yas < 24 && yas > 12) {
+
+                        System.out.println("Bilet Ücretiniz: " + indirim2 + "TL");
+
+                    } else if (yas >= 65) {
+
+                        System.out.println("Bilet Ücretiniz: " + indirim3 + "TL");
+
+                    } else if (yas < 65 && yas > 23){
+
+                        System.out.println("Bilet Ücretiniz: " + tutar + "TL");
+
+                    } else {
+
+                        System.out.println("Hatalı Veri Girdiniz!");
+                    }
+
+
+                } else {
+                    System.out.println("Hatalı Veri Girdiniz!");
+
+                }
+                break;
+
+
+            case 2:
+
+                if (km > 0) {
+
+
+                    if (yas > 0 && yas < 12) {
+
+                        System.out.println("Bilet Ücretiniz: " + ((indirim1-(indirim1 * 0.20))*2) + "TL");
+
+                    } else if (yas < 24 && yas > 12) {
+
+                        System.out.println("Bilet Ücretiniz: " + ((indirim2-(indirim2 * 0.20))*2) + "TL");
+
+                    } else if (yas >= 65 ) {
+
+                        System.out.println("Bilet Ücretiniz: " + ((indirim3-(indirim3 * 0.20))*2) + "TL");
+
+                    } else if (yas < 65 && yas > 23 ) {
+
+                        System.out.println("Bilet Ücretiniz: " + ((tutar-(tutar * 0.20))*2) + "TL");
+
+                    } else {
+
+                        System.out.println("Hatalı Veri Girdiniz!");
+                    }
+
+                } else {
+
+                    System.out.println("Hatalı Veri Girdiniz!");
+
+                }
+                break;
+
+            default:
+
+                System.out.println("Hatalı Veri Girdiniz!");
+
+
+        }
+    }
+}
