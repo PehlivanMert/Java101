33f83992d33bc98869b70fc9463e933793ebf8ec
PehlivanMert <pehlivannmert@gmail.com>
2023-11-02 15:43:24 +0300

    Ters Üçgen

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,27 @@
+//Mükemmel Sayı Nedir ?
+//Bir sayının kendisi hariç pozitif tam sayı çarpanları (kalansız bölen sayıların) toplamı kendisine eşit olan sayıya mükemmel sayı denir.
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner giris = new Scanner(System.in);
+        int n1;
+        int total = 0;
+
+
+        System.out.print("Bir Sayı Giriniz: ");
+        n1 = giris.nextInt();
+
+        for (int i = 1; i < n1; i++) {
+
+            if (n1 % i == 0) {
+                total += i;
+            }
+        } if (total==n1) {
+
+            System.out.println("Bu sayı Mükemmel Bir Sayı, bu sayının eşi benzeri yoktur wow :)");
+        } else {
+            System.out.println("Bu sayının mükemmellikle uzaktan yakından alakası yok :) ");
+        }
+    }
+}
+
