4bb3c6c0cf6d986de5516b87c5964a91747f6f02
PehlivanMert <pehlivannmert@gmail.com>
2023-11-08 23:34:53 +0300

    Create dikdörtgenAlaniHesaplama.java

---
+++ src/dikdörtgenAlaniHesaplama.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class dikdörtgenAlaniHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Dikdörtgenin kısa kenarını giriniz: ");
+        int kisaKenar = input.nextInt();
+        System.out.print("Dikdörtgenin uzun kenarını giriniz: ");
+        int uzunKenar = input.nextInt();
+        int alan = kisaKenar * uzunKenar;
+        System.out.println("Dikdörtgenin alanı: " + alan);
+    }
+}
