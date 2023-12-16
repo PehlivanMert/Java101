9f68a904f1bf6c8290741cedd9c502581f9b54e8
Mert <133760259+PehlivanMert@users.noreply.github.com>
2023-12-16 19:46:29 +0300

    Update BasamakDegerleriniToplama.java

--- src/BasamakDegerleriniToplama.java
+++ src/BasamakDegerleriniToplama.java
@@ -2,27 +2,18 @@
 import java.util.Scanner;
 public class BasamakDegerleriniToplama {
     public static void main(String[] args) {
-
-        Scanner girdi = new Scanner (System.in);
+        Scanner girdi = new Scanner(System.in);
         System.out.print("Bir Sayı Giriniz: ");
-            int sayi = girdi.nextInt();
-            int basamakSayisi = 0;
-            int tempSayi = sayi;
-            int basDegeri;
-            int sonuc = 0;
-            while (tempSayi != 0) {
-                tempSayi /= 10;
-                basamakSayisi++;
-            }
-            tempSayi = sayi;
-            while (tempSayi != 0) {
-                basDegeri = tempSayi % 10;
+        int sayi = girdi.nextInt();
+        int basDegeri;
+        int sonuc = 0;
 
-                for (int i = 1; i <= basamakSayisi; i++){}
-                sonuc += basDegeri;
+        while (sayi != 0) {
 
-                tempSayi /= 10;
-            }
+            basDegeri = sayi % 10;
+            sonuc += basDegeri;
+            sayi /= 10;
+        }
         System.out.println("Basamak Değerlerinin Toplamı: " + sonuc);
 
     }
